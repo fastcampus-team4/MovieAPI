ba00e87436a586e075f57a62483735f8a9aa295e
Wonny-ing <h.wonny11@gmail.com>
2022-10-27 18:59:11 +0900

    Modified searchBtn click event

--- main.js
+++ main.js
@@ -13,31 +13,6 @@ let title;
   page += 1;
   renderMovies(movies);
 
-  moreBtnEl.addEventListener('click', async () => {
-    const movies = await getMovies(title, page);
-    page += 1;
-    renderMovies(movies);
-  });
-
-  searchBtn.addEventListener('click', async (event) => {
-    event.preventDefault();
-    page = 1;
-    title = searchInput.value;
-    const movies = await getMovies(title, page);
-    moviesEl.innerHTML = '';
-    if (title) {
-      renderMovies(movies);
-    } else {
-      alert('error');
-    }
-  });
-
-  async function getMovies(title = 'avengers', page = 1) {
-    const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}`);
-    const { Search: movies } = await res.json();
-    return movies;
-  }
-
   function renderMovies(movies) {
     for (const movie of movies) {
       const el = document.createElement('div');
@@ -54,5 +29,44 @@ let title;
 
       moviesEl.append(el);
     }
+  }
+
+  async function getMovies(title = 'avengers', page = 1) {
+    const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}`);
+    const { Search: movies } = await res.json();
+    return movies;
+  }
+
+  // 더보기 버튼 클릭시, 영화 리스트 10개씩 나옴
+  moreBtnEl.addEventListener('click', async () => {
+    const movies = await getMovies(title, page);
+    page += 1;
+    renderMovies(movies);
+  });
+
+  // 검색 버튼 클릭시, input 값에 대한 영화 리스트 나옴
+  searchBtn.addEventListener('click', async (event) => {
+    // 새로고침 방지
+    event.preventDefault();
+    title = searchInput.value;
+
+    // search-input 값 초기화
+    searchInput.value = '';
+
+    initMovies();
+    const movies = await getMovies(title, page);
+    if (title) {
+      renderMovies(movies);
+      page += 1;
+    } else {
+      alert('error');
+    }
+  });
+
+  function initMovies() {
+    // movies 초기화
+    moviesEl.innerHTML = '';
+    // page 초기화
+    page = 1;
   }
 })();
