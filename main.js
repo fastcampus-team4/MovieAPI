f71c215ad00984ab83c81972f6bcbdb23d6109bb
Hyeeeein <khi_9866@naver.com>
2022-10-30 02:03:29 +0900

    221030 Complete Movie List, Release Year Features

--- main.js
+++ main.js
@@ -1,61 +1,19 @@
-/* (async () => {
-  const moviesEl = document.querySelector(".movies");
-  const srchInput = document.querySelector("input");
-  const srchBtn = document.querySelector("button");
-
-  srchBtn.addEventListener("click", async (e) => {
-    if (srchInput.value !== "") {
-      e.preventDefault();
-      const movies = await searchMovies(srchInput.value, 3);
-      renderMovies(movies);
-    }
-  });
-
-  async function searchMovies(srch = "", page = 1) {
-    const res = await fetch(
-      `https://omdbapi.com/?apikey=7035c60c&s=${srch}&page=${page}`
-    );
-    const { Search: movies, totalResults } = await res.json();
-    return movies;
-  }
-
-  function renderMovies(movies) {
-    for (const movie of movies) {
-      const el = document.createElement("div");
-      el.classList.add("movie");
-      const h1El = document.createElement("h1");
-      h1El.textContent = movie.Title;
-      h1El.addEventListener("click", () => {
-        console.log(movie.Title);
-      });
-      const imgEl = document.createElement("img");
-      imgEl.src = movie.Poster;
-      el.append(h1El, imgEl);
-
-      moviesEl.append(el);
-    }
-  }
-})(); */
-
 (async () => {
   // 초기화 코드들~
   const moviesEl = document.querySelector(".movies");
   const moreBtnEl = document.querySelector(".btn");
-  const inputEl = document.querySelector("input");
-  const srchBtn = document.querySelector("button");
+  const inputEl = document.querySelector(".search-form");
+  const srchBtn = document.querySelector(".search-btn");
+  const countOp = document.querySelector(".count-form");
+  const yearOp = document.querySelector(".year-form");
+
   let page = 1;
+  let srchTxt = "frozen";
 
   // 최초 호출!
-  /* const movies = await getMovies();
+  const movies = await getMovies();
   page += 1;
-  renderMovies(movies); */
-
-  // 더보기 버튼 클릭!
-  moreBtnEl.addEventListener("click", async () => {
-    const movies = await getMovies(inputEl.value, page);
-    page += 1;
-    renderMovies(movies);
-  });
+  renderMovies(movies);
 
   // 검색 입력 받아오기
   srchBtn.addEventListener("click", async (e) => {
@@ -64,36 +22,93 @@
     inputEl.value = "";
     moviesEl.innerHTML = "";
     page = 1;
+    const year = yearOp.value;
+    countVal = countOp.value;
 
     if (srchTxt) {
-      const movies = await getMovies(srchTxt, page);
+      const movies = await getMovies(srchTxt, year, page);
       renderMovies(movies);
+      for (let i = 1; i < countVal; i++) {
+        pageCntUp();
+      }
     }
   });
+  // 영화 목록 양 선택
+  /* countOp.forEach((el) => {
+    if ()
+  }); */
+  /* for (let i = 1; i <= countOp.length; i++) {
+    if (countOp.value === i) {
+      srchTxt = inputEl.value;
+      let year = yearOp.value;
+      // page += 1
+      const movies = await getMovies(srchTxt, year, countOp.value);
+      renderMovies(movies);
+    }
+  } */
 
-  async function getMovies(search = "", page = 1) {
-    const res = await fetch(
-      `https://omdbapi.com/?apikey=7035c60c&s=${search}&page=${page}`
-    );
+  // 더보기 버튼 클릭!
+  moreBtnEl.addEventListener("click", async () => {
+    pageCntUp();
+  });
+
+  // 개봉연도 검색
+  for (let i = 2022; i >= 1985; i--) {
+    const yearOptions = document.createElement("option");
+    yearOptions.value = i;
+    yearOptions.textContent = i;
+    yearOp.append(yearOptions);
+  }
+  const yearOptions = document.querySelectorAll(".year-form option");
+  let year = "";
+  let opsVal = yearOptions.value;
+  if (opsVal === year) {
+    const movies = await getMovies(srchTxt, opsVal, page);
+    renderMovies(movies);
+  }
+
+  // 페이지 더 가져오는 function
+  async function pageCntUp() {
+    page += 1;
+    const year = yearOp.value;
+    const movies = await getMovies(srchTxt, year, page); // 왜 인수를 넣으면 에러가 나지?
+    renderMovies(movies);
+  }
+
+  // api
+  async function getMovies(search = "frozen", year = "", page = 1) {
+    const url = `https://omdbapi.com/?apikey=7035c60c&s=${search}&y=${year}&page=${page}`;
+    // console.log(url);
+    const res = await fetch(url);
     const { Search: movies, totalResults } = await res.json();
     return movies;
   }
+
+  // 영화 목록 가져오는 function
   function renderMovies(movies) {
-    console.log(movies);
+    if (!movies) {
+      console.log("movies 가 제대로 안들어왔습니다.");
+      return;
+    }
+
     for (const movie of movies) {
       const el = document.createElement("div");
       el.classList.add("movie");
-
       const h1El = document.createElement("h1");
       h1El.textContent = movie.Title;
-      h1El.addEventListener("click", () => {
-        console.log(movie.Title);
-      });
       const imgEl = document.createElement("img");
       imgEl.src = movie.Poster;
-      el.append(h1El, imgEl);
+      // year
+      const yearEl = document.createElement("p");
+      yearEl.textContent = movie.Year;
+      el.append(h1El, imgEl, yearEl);
 
       moviesEl.append(el);
+
+      // 엑박 대체 사진은 넣었지만 엑박 표시 없애는 건 해결 못함
+      if (movie.Poster === "N/A") {
+        imgEl.classList.add("none");
+      }
     }
   }
 })();
