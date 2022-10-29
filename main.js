0751745bbf615d2e74130bee8cfd3d1a8edaae3e
hae9 <gorud0929@naver.com>
2022-10-29 11:55:07 +0900

    221029-개봉연도, 목록갯수 분류 검색기능 구현

--- main.js
+++ main.js
@@ -2,21 +2,21 @@
 const moviesEl = document.querySelector('.movies');
 const moreBtnEl = document.querySelector('.btn');
 const searchBtn = document.querySelector('button#searching');
-
 const movieList = document.querySelector('.movies');
+
 let page = 1;
 let title = '';
+let year = '';
 
 (async () => {
   // 최초 호출!
   const movies = await getMovies();
-  page += 1;
   renderMovies(movies);
 
   // 더보기 버튼 클릭!
   moreBtnEl.addEventListener('click', async () => {
-    const movies = await getMovies(page, title);
     page += 1;
+    const movies = await getMovies(title, page, year);
     renderMovies(movies);
   });
 
@@ -24,18 +24,22 @@ let title = '';
   searchBtn.addEventListener('click', async function () {
     movieList.innerHTML = '';
     title = document.querySelector('input#searching').value;
+    console.log(title);
+    console.log(page);
     if (title === '') {
       alert('제목을 입력해 주세요.');
     } else {
-      const movies = await getMovies(1, title);
-      renderMovies(movies);
+      for (i = 1; i <= page; i++) {
+        const movies = await getMovies(title, i, year);
+        renderMovies(movies);
+      }
     }
   });
 })();
 
-async function getMovies(page = 1, title = 'avengers') {
+async function getMovies(title = 'avengers', page = 1, year = '') {
   const res = await fetch(
-    `https://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}`
+    `https://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}&y=${year}`
   );
   const { Search: movies, totalResults } = await res.json();
   return movies;
@@ -69,4 +73,23 @@ function renderMovies(movies) {
 
     moviesEl.append(el);
   }
+}
+// 목록갯수 선택(어느페이지까지 불러올지)
+function selectedPage(paging) {
+  page = paging.value;
+  console.log(page);
+}
+
+// 개봉연도 선택
+function selectedYear(selectYear) {
+  year = selectYear.value;
+  console.log(year);
+}
+// 개봉연도 선택지 만들기
+for (i = 2022; i > 1984; i--) {
+  const selectYear = document.querySelector('.year');
+  const yearOpt = document.createElement('option');
+  yearOpt.value = i;
+  yearOpt.textContent = i;
+  selectYear.append(yearOpt);
 }
