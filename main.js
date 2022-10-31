cdeb8e841ed75e0031cac6864b92a4dea85a70c9
hae9 <gorud0929@naver.com>
2022-10-31 17:41:39 +0900

    221031-hash구현  뒤로가기, 홈버튼 기능 구현

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-// 초기화 코드들~
 const moviesEl = document.querySelector('.movies');
-const moreBtnEl = document.querySelector('.btn');
+const moreBtnEl = document.querySelector('.more');
 const searchBtn = document.querySelector('button#searching');
-const movieEl = document.querySelector('.movie');
 const movieA = document.querySelector('a');
-const bodyEl = document.querySelector('body');
+const moviesListEl = document.querySelector('.movies-list');
+const movieEl = document.querySelector('.movie-detail');
 
 let page = 1;
-let title = '';
-let year = '';
 
 (async () => {
   // 최초 호출!
@@ -18,50 +15,55 @@ let year = '';
   // 더보기 버튼 클릭!
   moreBtnEl.addEventListener('click', async () => {
     page += 1;
+    const title = document.querySelector('input#searching').value;
+    const year = document.querySelector('.year').value;
     const movies = await getMovies(title, page, year);
+    console.log(movies);
     renderMovies(movies);
   });
 
   // 검색 버튼 클릭
-  searchBtn.addEventListener('click', async function () {
-    moviesEl.innerHTML = '';
-    title = document.querySelector('input#searching').value;
-    console.log(title);
-    console.log(page);
-    if (title === '') {
-      alert('제목을 입력해 주세요.');
-    } else {
-      for (i = 1; i <= page; i++) {
-        const movies = await getMovies(title, i, year);
-        renderMovies(movies);
-      }
-    }
-  });
+  searchBtn.addEventListener('click', renderSearchList);
+
+  // 영화포스터 클릭(상세정보)
+  window.addEventListener('hashchange', router);
 })();
 
-// 영화포스터 클릭(상세정보)
-window.addEventListener('hashchange', async function () {
+async function router() {
+  const routePath = location.hash.substr(9);
+  if (routePath === '') {
+    console.log(routePath);
+    const movies = await getMovies();
+    renderMovies(movies);
+  } else if (routePath.indexOf('#/detail/' >= 0)) {
+    console.log(routePath);
+    await getDetail();
+  }
+}
+
+async function getDetail() {
   const id = location.hash.substr(9);
   const movie = await getMovie(id);
   renderMovie(movie);
-});
+}
 
-// async function getDetail() {
-//   const id = location.hash.substr(9);
-//   const movie = await getMovie(id);
-//   renderMovie(movie);
-// }
-
-// async function router() {
-//   const routePath = location.hash;
-
-//   if (routePath === '') {
-//     //최초호출 화면
-//     const movies = await getMovies();
-//     renderMovies(movies);
-//   } else if (routePath.indexOf('#/detail/') >= 0) {
-//   }
-// }
+// 검색목록 화면출력
+async function renderSearchList() {
+  moviesEl.innerHTML = '';
+  const title = document.querySelector('input#searching').value;
+  const page = document.querySelector('.paging').value;
+  const year = document.querySelector('.year').value;
+  console.log(title);
+  console.log(page);
+  if (title === '') {
+    alert('제목을 입력해 주세요.');
+  } else {
+    for (i = 1; i <= page; i++) {
+      const movies = await getMovies(title, i, year);
+      renderMovies(movies);
+    }
+  }
+}
 
 // 영화목록 불러오기
 async function getMovies(title = 'avengers', page = 1, year = '') {
@@ -69,10 +71,12 @@ async function getMovies(title = 'avengers', page = 1, year = '') {
     `https://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}&y=${year}`
   );
   const { Search: movies, totalResults } = await res.json();
+  console.log(movies, totalResults);
   return movies;
 }
 // 영화목록 화면 출력하기
 function renderMovies(movies) {
+  movieEl.innerHTML = '';
   for (const movie of movies) {
     const el = document.createElement('div');
     const movieHash = document.createElement('a');
@@ -100,9 +104,9 @@ function renderMovies(movies) {
     const imgEl = document.createElement('img');
     imgEl.src = movie.Poster;
     movieHash.append(h1El, imgEl);
-
     moviesEl.append(el);
   }
+  moreBtnEl.style.display = 'block';
 }
 
 // 영화 상세정보 불러오기
@@ -120,30 +124,50 @@ async function getMovie(id) {
 
 // 상세정보 화면출력
 function renderMovie(movie) {
-  bodyEl.innerHTML = '';
+  moviesEl.innerHTML = '';
+  movieEl.innerHTML = '';
+  moreBtnEl.style.display = 'none';
   const el = document.createElement('div');
-
+  el.classList.add('container');
+  el.style.paddingTop = '100px';
   el.innerHTML = /* html */ `
-    <img src="${movie.Poster}" />
-    <h1>${movie.Title}</h1>
-    <p>${movie.Year} ${movie.Runtime} ${movie.Country}</p>
-    <p>${movie.Plot}</p>
-
+    <div class='row justify-content-center'>
+      <div class='col-4'>
+        <img src="${movie.Poster}" />
+      </div>
+      <div class='col-6'>
+        <h1 style=>${movie.Title}</h1>
+        <p>${movie.Year} ${movie.Runtime} ${movie.Country}</p>
+        <p>${movie.Plot}</p>
+        <strong>Ratings</strong>
+        <!-- <img scr='https://raw.githubusercontent.com/ParkYoungWoong/vue3-movie-app/master/src/assets/Internet%20Movie%20Database.png' /> -->
+        <p>${movie.Ratings[0].Value}${movie.Ratings[1].Value}${movie.Ratings[2].Value}</p>
+        <strong>Actors</strong>
+        <p>${movie.Actors}</p>
+        <strong>Director</strong>
+        <p>${movie.Director}</p>
+        <strong>Production</strong>
+        <p>${movie.Production}</p>
+        <strong>Genre</strong>
+        <p>${movie.Genre}</p>
+      </div>
+    </div>
+    <button><a href=''>홈으로</a></button>
   `;
-  bodyEl.append(el);
+  movieEl.append(el);
 }
 
-// 목록갯수 선택(어느페이지까지 불러올지)
-function selectedPage(paging) {
-  page = paging.value;
-  console.log(page);
-}
+// // 목록갯수 선택(어느페이지까지 불러올지)
+// function selectedPage(paging) {
+//   page = paging.value;
+//   console.log(page);
+// }
 
-// 개봉연도 선택
-function selectedYear(selectYear) {
-  year = selectYear.value;
-  console.log(year);
-}
+// // 개봉연도 선택
+// function selectedYear(selectYear) {
+//   year = selectYear.value;
+//   console.log(year);
+// }
 
 // 개봉연도 선택지 만들기
 for (i = 2022; i > 1984; i--) {
