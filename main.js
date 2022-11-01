e125e747dd057777ab1a6f00fdc73825072cc78a
hae9 <gorud0929@naver.com>
2022-11-01 23:23:17 +0900

    221101 - 대체이미지 띄우기, 마크업 수정 스타일 적용

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const moreBtnEl = document.querySelector('.more');
 const searchBtn = document.querySelector('button#searching');
 const movieA = document.querySelector('a');
 const moviesListEl = document.querySelector('.movies-list');
-const movieEl = document.querySelector('.movie-detail');
+const movieDetailEl = document.querySelector('.movie-detail');
+const searchEl = document.querySelector('.search');
 
 let page = 1;
 
@@ -15,8 +16,11 @@ let page = 1;
   // 더보기 버튼 클릭!
   moreBtnEl.addEventListener('click', async () => {
     page += 1;
+    console.log(page);
     const title = document.querySelector('input#searching').value;
+    console.log(title);
     const year = document.querySelector('.year').value;
+    console.log(year);
     const movies = await getMovies(title, page, year);
     console.log(movies);
     renderMovies(movies);
@@ -30,22 +34,27 @@ let page = 1;
 })();
 
 async function router() {
-  const routePath = location.hash.substr(9);
+  const routePath = location.hash;
+  // console.log(routePath);
+  // console.log(routePath.indexOf('#/detail'));
   if (routePath === '') {
     console.log(routePath);
     const movies = await getMovies();
     renderMovies(movies);
-  } else if (routePath.indexOf('#/detail/' >= 0)) {
+  } else if (routePath.indexOf('#/detail') >= 0) {
     console.log(routePath);
-    await getDetail();
+    const movie = await getMovie();
+    renderMovie(movie);
+  } else if (routePath.indexOf('#/search') >= 0) {
+    const movies = await getMovies();
+    renderMovies(movies);
   }
 }
 
-async function getDetail() {
-  const id = location.hash.substr(9);
-  const movie = await getMovie(id);
-  renderMovie(movie);
-}
+// async function getDetail() {
+//   const movie = await getMovie();
+//   renderMovie(movie);
+// }
 
 // 검색목록 화면출력
 async function renderSearchList() {
@@ -66,7 +75,7 @@ async function renderSearchList() {
 }
 
 // 영화목록 불러오기
-async function getMovies(title = 'avengers', page = 1, year = '') {
+async function getMovies(title = '', page = 1, year = '') {
   const res = await fetch(
     `https://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}&y=${year}`
   );
@@ -76,7 +85,20 @@ async function getMovies(title = 'avengers', page = 1, year = '') {
 }
 // 영화목록 화면 출력하기
 function renderMovies(movies) {
-  movieEl.innerHTML = '';
+  console.log('목록화면 출력');
+  moviesListEl.style.display = 'block';
+  searchEl.style.display = 'flex';
+  movieDetailEl.style.display = 'none';
+
+  if (movies === undefined) {
+    moviesEl.innerHTML = '';
+    console.log('movies가 언디파인드');
+    const el = document.createElement('div');
+    el.classList.add('alert');
+    el.textContent = 'Search for the movie title !';
+    moviesEl.append(el);
+    return;
+  }
   for (const movie of movies) {
     const el = document.createElement('div');
     const movieHash = document.createElement('a');
@@ -96,21 +118,24 @@ function renderMovies(movies) {
     // })
 
     // Type 2
-    const h1El = document.createElement('h1');
-    h1El.textContent = movie.Title;
-    h1El.addEventListener('click', () => {
+    const divEl = document.createElement('div');
+    divEl.textContent = movie.Title;
+    divEl.addEventListener('click', () => {
       console.log(movie.Title);
     });
     const imgEl = document.createElement('img');
-    imgEl.src = movie.Poster;
-    movieHash.append(h1El, imgEl);
+    imgEl.src =
+      movie.Poster === 'N/A'
+        ? `https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/495px-No-Image-Placeholder.svg.png?20200912122019`
+        : movie.Poster;
+    movieHash.append(imgEl, divEl);
     moviesEl.append(el);
   }
-  moreBtnEl.style.display = 'block';
 }
 
 // 영화 상세정보 불러오기
-async function getMovie(id) {
+async function getMovie() {
+  const id = location.hash.substr(9) ? location.hash.substr(9) : 'tt2294629';
   const res = await fetch(
     `https://omdbapi.com/?apikey=7035c60c&i=${id}&plot=full`
   );
@@ -122,39 +147,39 @@ async function getMovie(id) {
   return json.Error;
 }
 
+const poster = document.querySelector('.poster');
+const title = document.querySelector('.title');
+const info = document.querySelector('.info');
+const plot = document.querySelector('.plot');
+const ratings = document.querySelector('.ratings');
+const actors = document.querySelector('.actors');
+const director = document.querySelector('.director');
+const production = document.querySelector('.production');
+const genre = document.querySelector('.genre');
+
 // 상세정보 화면출력
 function renderMovie(movie) {
-  moviesEl.innerHTML = '';
-  movieEl.innerHTML = '';
-  moreBtnEl.style.display = 'none';
-  const el = document.createElement('div');
-  el.classList.add('container');
-  el.style.paddingTop = '100px';
-  el.innerHTML = /* html */ `
-    <div class='row justify-content-center'>
-      <div class='col-4'>
-        <img src="${movie.Poster}" />
-      </div>
-      <div class='col-6'>
-        <h1 style=>${movie.Title}</h1>
-        <p>${movie.Year} ${movie.Runtime} ${movie.Country}</p>
-        <p>${movie.Plot}</p>
-        <strong>Ratings</strong>
-        <!-- <img scr='https://raw.githubusercontent.com/ParkYoungWoong/vue3-movie-app/master/src/assets/Internet%20Movie%20Database.png' /> -->
-        <p>${movie.Ratings[0].Value}${movie.Ratings[1].Value}${movie.Ratings[2].Value}</p>
-        <strong>Actors</strong>
-        <p>${movie.Actors}</p>
-        <strong>Director</strong>
-        <p>${movie.Director}</p>
-        <strong>Production</strong>
-        <p>${movie.Production}</p>
-        <strong>Genre</strong>
-        <p>${movie.Genre}</p>
-      </div>
-    </div>
-    <button><a href=''>홈으로</a></button>
-  `;
-  movieEl.append(el);
+  moviesListEl.style.display = 'none';
+  searchEl.style.display = 'none';
+  movieDetailEl.style.display = 'block';
+
+  poster.src = movie.Poster === 'N/A' ? poster.src : movie.Poster;
+
+  title.textContent = movie.Title;
+
+  info.textContent = `${movie.Year} ▪ ${movie.Runtime} ▪ ${movie.Country}`;
+
+  plot.textContent = movie.Plot;
+
+  // ratings.textContent = `Internet Movie Database: ${movie.Ratings[0].Value} Rotten Tomatoes: ${movie.Ratings[1].Value} Metacritic: ${movie.Ratings[2].Value}`;
+
+  actors.textContent = movie.Actors;
+
+  director.textContent = movie.Director;
+
+  production.textContent = movie.Production;
+
+  genre.textContent = movie.Genre;
 }
 
 // // 목록갯수 선택(어느페이지까지 불러올지)
