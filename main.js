75b081432a66cf0f693e17e945c6e9f98b46030e
hae9 <gorud0929@naver.com>
2022-11-06 22:49:31 +0900

    221106 - readme 작성, js 수정

--- main.js
+++ main.js
@@ -22,21 +22,7 @@ let page = 1;
   renderMovies(movies);
 
   // 더보기 버튼 클릭!
-  moreBtnEl.addEventListener('click', async () => {
-    moreLoadingSpin.classList.remove('hidden');
-    page++;
-    const year = document.querySelector('.year').value;
-    console.log(title, page, year);
-    const movies = await getMovies(title, page, year);
-    moreLoadingSpin.classList.add('hidden');
-    if (!movies) {
-      console.log(title, page, year, movies);
-      alert('더 이상 불러올 목록이 없습니다.');
-    } else {
-      console.log(movies);
-      renderMovies(movies);
-    }
-  });
+  moreBtnEl.addEventListener('click', moreList);
 
   // 검색 버튼 클릭
   searchBtn.addEventListener('click', renderSearchList);
@@ -45,6 +31,7 @@ let page = 1;
   window.addEventListener('hashchange', router);
 })();
 
+// 해시변경시 불러올 화면 구분
 async function router() {
   const routePath = location.hash;
   // console.log(routePath);
@@ -62,13 +49,63 @@ async function router() {
   }
 }
 
-// 검색목록 화면출력
+// 영화목록 불러오기
+async function getMovies(title = '', page = 1, year = '') {
+  const res = await fetch(
+    `https://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}&y=${year}`
+  );
+  const { Search: movies, totalResults } = await res.json();
+  console.log(movies, totalResults);
+  return movies;
+}
+
+// 영화목록 화면 출력하기
+function renderMovies(movies) {
+  console.log('목록화면 출력');
+
+  moviesListEl.style.display = 'block';
+  searchEl.style.display = 'flex';
+  footerEl.style.display = 'block';
+  movieDetailEl.style.display = 'none';
+
+  if (movies === undefined) {
+    console.log('movies가 언디파인드');
+    moviesEl.innerHTML = '';
+    textEl.classList.remove('hidden'); // 언디파인드면 목록 지우고 초기화면 텍스트 띄우기
+    return;
+  }
+
+  for (const movie of movies) {
+    // 목록이 있다면 포스터와 영화제목 가져와서 append
+    const el = document.createElement('div');
+    const movieHash = document.createElement('a');
+    movieHash.href = `#/detail/${movie.imdbID}`;
+    el.append(movieHash);
+    el.classList.add('movie');
+
+    const divEl = document.createElement('div');
+    divEl.textContent = movie.Title;
+    divEl.classList.add('text-truncate');
+    divEl.addEventListener('click', () => {
+      console.log(movie.Title);
+    });
+    const imgEl = document.createElement('img');
+    imgEl.src =
+      movie.Poster === 'N/A'
+        ? `https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/495px-No-Image-Placeholder.svg.png?20200912122019`
+        : movie.Poster;
+    movieHash.append(imgEl, divEl);
+    moviesEl.append(el);
+  }
+}
+
+// 영화제목 검색했을 때 화면출력
 async function renderSearchList() {
   if (moviesEl) {
+    // 이전 검색목록 지우기
     moviesEl.innerHTML = '';
   }
-  // const searchInputEl = document.querySelector('input#searching');
-  // const title = searchInputEl.value;
+
   textEl.classList.add('hidden');
   loadingSpin.classList.remove('hidden');
   page = 1;
@@ -96,67 +133,26 @@ async function renderSearchList() {
   }
 
   loadingSpin.classList.add('hidden');
-
-  console.log(title);
 }
 
-// 영화목록 불러오기
-async function getMovies(title = '', page = 1, year = '') {
-  const res = await fetch(
-    `https://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}&y=${year}`
-  );
-  const { Search: movies, totalResults } = await res.json();
-  console.log(movies, totalResults);
-  return movies;
-}
+// 목록 더 불러오기
+async function moreList() {
+  // 초기화면 무한스크롤 적용시 그냥 리턴되게
+  if (title === '') return;
 
-// 영화목록 화면 출력하기
-function renderMovies(movies) {
-  console.log('목록화면 출력');
-
-  moviesListEl.style.display = 'block';
-  searchEl.style.display = 'flex';
-  footerEl.style.display = 'block';
-  movieDetailEl.style.display = 'none';
-
-  if (movies === undefined) {
-    console.log('movies가 언디파인드');
-    moviesEl.innerHTML = '';
-    textEl.classList.remove('hidden');
-    return;
-  }
-
-  for (const movie of movies) {
-    const el = document.createElement('div');
-    const movieHash = document.createElement('a');
-    movieHash.href = `#/detail/${movie.imdbID}`;
-    el.append(movieHash);
-    el.classList.add('movie');
-
-    // Type 1
-    // el.innerHTML = /* html */ `
-    //   <h1>${movie.Title}</h1>
-    //   <img src="${movie.Poster}" />
-    // `
-    // const h1El = el.querySelector('h1')
-    // h1El.addEventListener('click', () => {
-    //   console.log(movie.Title)
-    // })
-
-    // Type 2
-    const divEl = document.createElement('div');
-    divEl.textContent = movie.Title;
-    divEl.classList.add('text-truncate');
-    divEl.addEventListener('click', () => {
-      console.log(movie.Title);
-    });
-    const imgEl = document.createElement('img');
-    imgEl.src =
-      movie.Poster === 'N/A'
-        ? `https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/495px-No-Image-Placeholder.svg.png?20200912122019`
-        : movie.Poster;
-    movieHash.append(imgEl, divEl);
-    moviesEl.append(el);
+  moreLoadingSpin.classList.remove('hidden');
+  page++;
+  const year = document.querySelector('.year').value;
+  console.log(page, title, year);
+  const movies = await getMovies(title, page, year);
+  moreLoadingSpin.classList.add('hidden');
+  if (!movies) {
+    // 더이상 목록, 페이지가 없을 때
+    console.log(title, page, year, movies);
+    alert('더 이상 불러올 목록이 없습니다.');
+  } else {
+    console.log(movies);
+    renderMovies(movies);
   }
 }
 
@@ -165,6 +161,7 @@ const skeletons = document.querySelectorAll('.skeletons');
 
 // 영화 상세정보 불러오기
 async function getMovie() {
+  // 불필요한 화면 지우고 로딩애니메이션/ 스켈레톤ui 띄우기
   moviesListEl.style.display = 'none';
   footerEl.style.display = 'none';
   searchEl.style.display = 'none';
@@ -172,6 +169,7 @@ async function getMovie() {
   movieDetailEl.style.display = 'block';
   detailLoadingSpin.classList.remove('hidden');
   skeletons.forEach((ele) => ele.classList.remove('hidden'));
+  // 영화 id값 가져오기
   const id = location.hash.substr(9) ? location.hash.substr(9) : 'tt2294629';
   const res = await fetch(
     `https://omdbapi.com/?apikey=7035c60c&i=${id}&plot=full`
@@ -249,22 +247,7 @@ const callback = (entries) => {
     // console.log(entry);
     if (entry.isIntersecting) {
       console.log(entry);
-      (async () => {
-        if (title === '') return;
-        moreLoadingSpin.classList.remove('hidden');
-        page++;
-        const year = document.querySelector('.year').value;
-        console.log(page, title, year);
-        const movies = await getMovies(title, page, year);
-        moreLoadingSpin.classList.add('hidden');
-        if (!movies) {
-          console.log(title, page, year, movies);
-          alert('더 이상 불러올 목록이 없습니다.');
-        } else {
-          console.log(movies);
-          renderMovies(movies);
-        }
-      })();
+      moreList();
     }
   });
 };
